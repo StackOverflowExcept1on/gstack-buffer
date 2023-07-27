28532d9e231aa7909dd424dad34030a926fa84d3
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2023-07-27 18:04:50 +0300

    rewrite library in better way

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 #![no_std]
 
-#[cfg(feature = "alloc")]
 extern crate alloc;
+
+use alloc::vec::Vec;
 
 use core::ffi::c_void;
 use core::mem::{ManuallyDrop, MaybeUninit};
@@ -9,7 +10,7 @@ use core::slice::from_raw_parts_mut;
 
 const MAX_BUFFER_SIZE: usize = 64 * 1024;
 
-type Callback = unsafe extern "C" fn(ptr: *mut u8, data: *mut c_void);
+type Callback = unsafe extern "C" fn(ptr: *mut MaybeUninit<u8>, data: *mut c_void);
 
 #[cfg(target_arch = "wasm32")]
 extern "C" {
@@ -19,16 +20,19 @@ extern "C" {
 #[cfg(not(target_arch = "wasm32"))]
 unsafe extern "C" fn c_with_alloca(_size: usize, callback: Callback, data: *mut c_void) {
     let mut buffer = MaybeUninit::<[MaybeUninit<u8>; MAX_BUFFER_SIZE]>::uninit().assume_init();
-    callback(buffer.as_mut_ptr() as *mut _, data);
+    callback(buffer.as_mut_ptr(), data);
 }
 
 fn with_alloca<T>(size: usize, f: impl FnOnce(&mut [MaybeUninit<u8>]) -> T) -> T {
     #[inline(always)]
-    fn get_trampoline<F: FnMut(*mut u8)>(_closure: &F) -> Callback {
+    fn get_trampoline<F: FnMut(*mut MaybeUninit<u8>)>(_closure: &F) -> Callback {
         trampoline::<F>
     }
 
-    unsafe extern "C" fn trampoline<F: FnMut(*mut u8)>(ptr: *mut u8, data: *mut c_void) {
+    unsafe extern "C" fn trampoline<F: FnMut(*mut MaybeUninit<u8>)>(
+        ptr: *mut MaybeUninit<u8>,
+        data: *mut c_void,
+    ) {
         let f = &mut *(data as *mut F);
         f(ptr);
     }
@@ -37,7 +41,7 @@ fn with_alloca<T>(size: usize, f: impl FnOnce(&mut [MaybeUninit<u8>]) -> T) -> T
     let mut ret = MaybeUninit::uninit();
 
     let mut closure = |ptr| unsafe {
-        let slice = from_raw_parts_mut(ptr as *mut _, size);
+        let slice = from_raw_parts_mut(ptr, size);
         ret.write(ManuallyDrop::take(&mut f)(slice));
     };
 
@@ -50,9 +54,9 @@ fn with_alloca<T>(size: usize, f: impl FnOnce(&mut [MaybeUninit<u8>]) -> T) -> T
 }
 
 pub fn with_byte_buffer<T>(size: usize, f: impl FnOnce(&mut [MaybeUninit<u8>]) -> T) -> T {
-    #[cfg(feature = "alloc")]
-    if size > MAX_BUFFER_SIZE {
-        return f(alloc::vec::Vec::with_capacity(size).spare_capacity_mut());
+    if size <= MAX_BUFFER_SIZE {
+        with_alloca(size, f)
+    } else {
+        f(Vec::with_capacity(size).spare_capacity_mut())
     }
-    with_alloca(size, f)
 }
